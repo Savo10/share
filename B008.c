a8bfb0d08498795ef69586b956fb26d82d12178c
Savo10 <43596505+Savo10@users.noreply.github.com>
2019-12-17 12:36:52 +0900

    テスト　アップロード
    テキトウなソースコードをアップロード

---
+++ B008.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int i, j;
+int risk[1000][1000];
+int live_risk[1000];
+
+int ()
+{
+	if(risk_sum > )
+
+int main(void)
+{
+	int live, member;
+	int risk_sum;
+	int max = -1;
+	
+	scanf("%d", &member);
+	if(live == 0)
+		max = 0;
+	else{
+		scanf("%d", &live);
+		if(member == 0)
+			max = 0;
+	}
+	
+	if(max == -1){
+		for(i=0; i<live; i++)
+			for(j=0; j<member; j++)
+				scanf("%d", &risk[i][j]);
+	}
+	
+	for(i=0; i<live; i++){
+		risk_sum = 0;
+		for(j=0; j<member; j++)
+			live_risk[i] += risk[i][j];
+	}
+	
+	
